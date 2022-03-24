398be75622ecc25c3ae96cd4d2efab1fb4a18641
Sergey Andreev <aandreev.sm@gmail.com>
2022-03-24 19:44:01 +0300

    Fixed gulp command

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const purgecss = require('gulp-purgecss')
 
 gulp.task('purgecss', () => {
-  return gulp.src('css/*.css')
+  return gulp.src('css/main.css')
     .pipe(purgecss({
       content: ['public/index.html']
     }))
